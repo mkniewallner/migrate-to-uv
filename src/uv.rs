effac6c288787d80f26c1e3ab9b8fde77da9b603
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2025-12-24 23:14:47 +0100

    refactor: move `lock_dependencies` to `uv` module

--- src/uv.rs
+++ src/uv.rs
@@ -1,7 +1,9 @@
-use log::error;
+use log::{error, info, warn};
 use owo_colors::OwoColorize;
-use std::path::PathBuf;
+use std::io::ErrorKind;
+use std::path::{Path, PathBuf};
 use std::process;
+use std::process::{Command, Stdio};
 use which::which;
 
 const UV_EXECUTABLE: &str = "uv";
@@ -23,4 +25,65 @@ pub fn ensure_executable_exists() {
         "--skip-lock".bold(),
     );
     process::exit(1);
+}
+
+/// Lock dependencies with uv by running `uv lock` command.
+pub fn lock_dependencies(project_path: &Path, is_removing_constraints: bool) -> Result<(), ()> {
+    const UV_EXECUTABLE: &str = "uv";
+
+    match Command::new(UV_EXECUTABLE)
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+    {
+        Ok(_) => {
+            info!(
+                "Locking dependencies with \"{}\"{}...",
+                format!("{UV_EXECUTABLE} lock").bold(),
+                if is_removing_constraints {
+                    " again to remove constraints"
+                } else {
+                    ""
+                }
+            );
+
+            Command::new(UV_EXECUTABLE)
+                .arg("lock")
+                .current_dir(project_path)
+                .spawn()
+                .map_or_else(
+                    |_| {
+                        error!(
+                            "Could not invoke \"{}\" command.",
+                            format!("{UV_EXECUTABLE} lock").bold()
+                        );
+                        Err(())
+                    },
+                    |lock| match lock.wait_with_output() {
+                        Ok(output) => {
+                            if output.status.success() {
+                                Ok(())
+                            } else {
+                                Err(())
+                            }
+                        }
+                        Err(e) => {
+                            error!("{e}");
+                            Err(())
+                        }
+                    },
+                )
+        }
+        Err(e) if e.kind() == ErrorKind::NotFound => {
+            warn!(
+                "Could not find \"{}\" executable, skipping locking dependencies.",
+                UV_EXECUTABLE.bold()
+            );
+            Ok(())
+        }
+        Err(e) => {
+            error!("{e}");
+            Err(())
+        }
+    }
 }
